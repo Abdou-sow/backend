7dfa213abc33a9c5c6e856d53b2b96478c69cdf1
Abdou-sow <you@example.com>
2021-03-31 12:28:39 +0200

    backend exo 1,2,3

---
+++ chapitre_01/authors/exo3.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+const books=["Beowulf","Hamlet, Othello, Romeo and Juliet, MacBeth","Oliver Twist, A Christmas Carol","The Picture of Dorian Gray, The Importance of Being Earnest"]
+app.get('/authors/i/books/', (req, res) => {
+    res.send(books[i-1]);
+});
+
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+});
