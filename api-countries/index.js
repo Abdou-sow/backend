57dfa2016df0447e611038954adb390b7994a602
Abdou-sow <you@example.com>
2021-04-02 17:19:12 +0200

    Countries AJAX / Backend - Backend

---
+++ api-countries/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/country', (req, res) => {
+    var array = ["senegal","france","usa","italy","belgique"]
+    res.send(array);
+  });
+
+app.listen(port, () => {
+  console.log('Server started on port: ' + port);
+});
+
--- chapitre_01/authors/exo3.js
+++ chapitre_01/authors/exo3.js
@@ -3,9 +3,10 @@ const port = 8000;
 
 const app = express();
 
-const books=["Beowulf","Hamlet, Othello, Romeo and Juliet, MacBeth","Oliver Twist, A Christmas Carol","The Picture of Dorian Gray, The Importance of Being Earnest"]
+//const books = ["Beowulf", "Hamlet, Othello, Romeo and Juliet, MacBeth", "Oliver Twist, A Christmas Carol", "The Picture of Dorian Gray, The Importance of Being Earnest"]
+
 app.get('/authors/i/books/', (req, res) => {
-    res.send(books[i-1]);
+    res.send(books[i - 1]);
 });
 
 app.listen(port, () => {
