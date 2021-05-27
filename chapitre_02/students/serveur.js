f76427db9e06dd9da16f7644d8a4dc8fc22bd86e
Abdou sow <sowabdou2009@gmail.com>
2021-05-27 16:04:11 +0200

    server revue

--- chapitre_02/students/serveur.js
+++ chapitre_02/students/serveur.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const cors = require("cors");
-const studentsData = require('./datastudents.js')
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+const port = 8000
+
+const app = express()
+
 app.use(cors())
+app.use(express.json())
 
-const port = 9000;
+const students = []
 
+app.get("/students", (req, res) => {
+    res.json(students)
+})
 
-// les Route
-app.get('/', (req, res) => {
-    res.json(studentsData);
-});
-// Run server
+app.post("/students", (req, res) => {
+    const newStudent = req.body
+
+    students.push(newStudent)
+
+    res.json({
+        message: "post students ok",
+        newStudent
+    })
+})
+
+app.get("*", (req, res) => {
+    res.json({
+        message: "The route doesn't exist"
+    })
+})
+
 app.listen(port, () => {
-    console.log('Server started on port: ' + port);
-});
+    console.log(`J'écoute des requêtes sur le port ${port}`);
+})
