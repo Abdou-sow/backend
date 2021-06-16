d4aa0b3a75def74fa16891c618a3e6c2f136c4c0
Abdou-sow <sowabdou2009@gmail.com>
2021-06-16 16:28:05 +0200

    validator troisieme etape base de donnee

--- chapitre_04/server.js
+++ chapitre_04/server.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const expressValidator = require("express-validator");
+const mongoose = require("mongoose")
+
+
+mongoose.connect("mongodb://localhost:27017/validator", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("I'm connected to the database");
+    }
+})
+
+const usersSchema = mongoose.Schema({
+    userName: String,
+    age: Number,
+    ville: String,
+    date: { type: Date, default: Date.now }
+})
+
+const User = mongoose.model("User", usersSchema)
+
 
 
 const port = 8000;
@@ -11,34 +31,54 @@ app.use(express.json());
 
 app.post("/validator",
     expressValidator.body("username").isLength(4),
-    expressValidator.body("username").isLength(4),
-    expressValidator.body("La ville de l'utilisateur").optional().isIn(["Paris", "Tokyo", "Los Angeles"]),
+    expressValidator.body("age").isLength(2),
+    expressValidator.body("ville").optional().isIn(["Paris", "Tokyo", "Los Angeles"]),
 
-    (req, res) => {
-        const errors = expressValidator.validationResult(req);
+    async (req, res) => {
+        const errors = await expressValidator.validationResult(req);
 
         if (!errors.isEmpty()) {
-            // res.json({message :"il ya une erreur"})
-            return res.status(400).json(
-                {
-                    "errors": [
-                        {
-                            "location": "body",
-                            "msg": "Invalid value",
-                            "param": "username"
-                        }
-                    ]
-                }
-            );
-        } else {
+
+        return res.status(400).json(
+            {
+                "errors": [
+                    {
+                        "location": "body",
+                        "msg": "Invalid value",
+                        "param": "username"
+                    }
+                ]
+            }
+        );
+    } else {
+        try {
+            const newUser = req.body
+
+            const user = new User({
+                userName: newUser.username,
+                age: newUser.age,
+                ville: newUser.ville,
+
+            })
+
+            const usersaved = await user.save()
+
             res.json({
-                message: "ok"
+                message: "The user was saved correctly",
+                newUser
+            })
+
+
+        } catch (error) {
+            console.error("Error in POST /Validator", error)
+
+            res.json({
+                message: "The user was not saved :("
             })
         }
 
     }
-)
-
+})
 
 // la route GET / qui enverra tous les utilisateurs
 
@@ -75,7 +115,12 @@ app.get("/users/email", (req, res) => {
     })
 })
 
-
+//pour toutes les routes inhesitante
+app.get("*", (req, res) => {
+    res.json({
+        message: "The route doesn't exist"
+    })
+})
 
 
 app.listen(port, () => {
