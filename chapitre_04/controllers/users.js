13006f6bce6e81a700b51f3c24f820b4f3897500
Abdou-sow <sowabdou2009@gmail.com>
2021-06-18 16:50:15 +0200

    validator fini

--- chapitre_04/controllers/users.js
+++ chapitre_04/controllers/users.js
@@ -26,8 +26,8 @@ router.get("/users/userName/:userName", async (req, res) => {
     try {
         const nameUser = req.params.userName
         const user = await UserModel.find().exec()
-    
-        if (user.map(x =>x.userName).includes(nameUser)) {
+
+        if (user.map(x => x.userName).includes(nameUser)) {
 
             for (var i = 0; i < user.length; i++) {
 
@@ -49,25 +49,54 @@ router.get("/users/userName/:userName", async (req, res) => {
 
 })
 
+
 router.get("/users/id/:id", async (req, res) => {
     const idUser = req.params.id
     const user = await UserModel.find().exec()
 
     try {
-        if(user.map(x =>x.id).includes(idUser)){
-    
-        const userId = await UserModel.findById(idUser).exec()
+        if (user.map(x => x.id).includes(idUser)) {
 
-        res.json(userId)
-    }else{
-        res.json({
-            message: "id not found"
-        })
-    }
+            const userId = await UserModel.findById(idUser).exec()
+
+            res.json(userId)
+        } else {
+            res.json({
+                message: "id not found"
+            })
+        }
 
     } catch (err) {
 
         console.error(err)
+        res.status(500).json({ errorMessage: "There was a problem :(" })
+    }
+})
+
+router.get("/users/email/:email", async (req, res) => {
+
+    try {
+        const emailUser = req.params.email
+        const user = await UserModel.find().exec()
+        // console.log("user.map(x => x.email :",user.map(x => x.email));
+
+        if (user.map(x => x.email).includes(emailUser)) {
+
+            for (var i = 0; i < user.length; i++) {
+
+                if (user[i].email.toLowerCase() === emailUser) {
+
+                    res.json(user[i])
+                }
+            }
+        } else {
+            res.json({
+                message: "email not found"
+            })
+        }
+    } catch (err) {
+        console.error(err)
+
         res.status(500).json({ errorMessage: "There was a problem :(" })
     }
 })
@@ -100,6 +129,7 @@ router.post("/users/add",
                 const newUser = req.body
                 const user = new UserModel({
                     userName: newUser.username,
+                    email:newUser.email,
                     age: newUser.age,
                     ville: newUser.ville,
 
@@ -109,7 +139,7 @@ router.post("/users/add",
 
                 res.json({
                     message: "The user was saved correctly",
-                    newUser
+                    usersaved 
                 })
 
 
@@ -123,6 +153,9 @@ router.post("/users/add",
 
         }
     })
+
+
+
 
 
 module.exports = router;
--- chapitre_04/server.js
+++ chapitre_04/server.js
@@ -25,31 +25,26 @@ app.use(express.json());
 
 // la route GET / qui enverra tous les utilisateurs
 
-app.use("/",userRoutes)
+app.use("/", userRoutes)
 
 
 // la route POST /users/add qui ajoutera un user (faites donc la validation de données avant d’enregistrer dans la bd)
 
-app.use("/users/add",userRoutes)
-    
+app.use("/users/add", userRoutes)
+
 // la route GET /users/:username qui enverra les infos d'un user selon son username
 
-app.use("/users/userName/:userName", userRoutes )
+app.use("/users/userName/:userName", userRoutes)
 
 
 // bonus 
 
-app.use("/users/id/:id" , userRoutes)
+app.use("/users/id/:id", userRoutes)
 
 
 // la route GET /users/:email qui enverra les infos d'un user selon son email
 
-app.get("/users/email", (req, res) => {
-
-    res.json({
-        message: "users/username ok"
-    })
-})
+app.use("/users/email/:email", userRoutes)
 
 
 //pour toutes les routes inhesitante
